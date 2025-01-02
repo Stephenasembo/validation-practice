9cc1c221a052344a3e5581a4c042d9862e602c56
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-02 23:33:53 +0300

    Check For Blanks When Element Loses Focus
    When user interacts with form control without providing a value an error
    will be displayed

--- script.js
+++ script.js
@@ -5,36 +5,55 @@ const password = document.querySelector('#password');
 const passwordConfirmation = document.querySelector('#passwordConfirmation');
 const submitBtn = document.querySelector('#submit');
 
+let inputElements = document.querySelectorAll('input');
+inputElements = Array.from(inputElements);
+
 submitBtn.addEventListener('click', checkBlanks);
 
-function checkBlanks(event){
-  event.preventDefault();
-  if (!country.value.length) {
-    let errorMsg = 'Please enter the name of your country';
-    displayError('#countryPara', errorMsg);
-    return;
-  }
-  if (!email.value.length) {
-    let errorMsg = 'Please enter your email address';
-    displayError('#emailPara', errorMsg);
-    return;
-  }
-  if (!zipCode.value.length) {
-    let errorMsg = 'Please enter your zip code';
-    displayError('#zipCodePara', errorMsg);
-    return;
-  }
-  if (!password.value.length) {
-    let errorMsg = 'Please enter your password';
-    displayError('#cpasswordPara', errorMsg);
-    return;
-  }
-  if (!passwordConfirmation.value.length) {
-    let errorMsg = 'Please confirm password entered';
-    displayError('#passConfirmPara', errorMsg);
-    return;
+function checkBlanks(event, input){
+  if (event) {
+    event.preventDefault();
   }
 
+  if (input === 'country'){
+    if (!country.value.length) {
+      let errorMsg = 'Please enter the name of your country';
+      displayError('#countryPara', errorMsg);
+      return;
+    }
+  }
+
+  else if (input === 'email'){
+    if (!email.value.length) {
+      let errorMsg = 'Please enter your email address';
+      displayError('#emailPara', errorMsg);
+      return;
+    }
+  }
+
+  else if (input === 'zipCode'){
+    if (!zipCode.value.length) {
+      let errorMsg = 'Please enter your zip code';
+      displayError('#zipCodePara', errorMsg);
+      return;
+    }
+  }
+
+  else if (input === 'password'){
+    if (!password.value.length) {
+      let errorMsg = 'Please enter your password';
+      displayError('#passwordPara', errorMsg);
+      return;
+    }
+  }
+
+  else if (input === 'passwordConfirmation'){
+    if (!passwordConfirmation.value.length) {
+      let errorMsg = 'Please confirm password entered';
+      displayError('#passConfirmPara', errorMsg);
+      return;
+    }
+  }
 }
 
 function displayError(paragraph, text){
@@ -42,4 +61,36 @@ function displayError(paragraph, text){
   const divError = document.createElement('div');
   divError.textContent = text;
   para.appendChild(divError);
+}
+
+inputElements.forEach((element) => {
+  element.addEventListener('blur', validateElement);
+})
+
+function validateElement(event) {
+  let input = event.target.id;
+  switch (input){
+    case 'country':
+      checkBlanks(event = '', 'country');
+      break;
+
+    case 'email':
+      checkBlanks(event = '', 'email');
+      if (email.validity.typeMismatch) {
+        displayError('#emailPara', errorMsg);
+      }
+      break;
+
+    case 'zipCode':
+      checkBlanks(event = '', 'zipCode');
+      break;
+      
+    case 'password':
+      checkBlanks(event = '', 'password');
+      break;
+
+    case 'passwordConfirmation':
+      checkBlanks(event = '', 'passwordConfirmation');
+      break;      
+  }
 }
