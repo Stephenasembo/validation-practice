91bc5cbaf828d40ea43f613d56b7265c0e640a31
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-02 22:20:45 +0300

    Handle Empty Inputs

--- script.js
+++ script.js
@@ -10,19 +10,36 @@ submitBtn.addEventListener('click', checkBlanks);
 function checkBlanks(event){
   event.preventDefault();
   if (!country.value.length) {
+    let errorMsg = 'Please enter the name of your country';
+    displayError('#countryPara', errorMsg);
     return;
   }
   if (!email.value.length) {
+    let errorMsg = 'Please enter your email address';
+    displayError('#emailPara', errorMsg);
     return;
   }
   if (!zipCode.value.length) {
+    let errorMsg = 'Please enter your zip code';
+    displayError('#zipCodePara', errorMsg);
     return;
   }
   if (!password.value.length) {
+    let errorMsg = 'Please enter your password';
+    displayError('#cpasswordPara', errorMsg);
     return;
   }
   if (!passwordConfirmation.value.length) {
+    let errorMsg = 'Please confirm password entered';
+    displayError('#passConfirmPara', errorMsg);
     return;
   }
 
+}
+
+function displayError(paragraph, text){
+  const para = document.querySelector(paragraph);
+  const divError = document.createElement('div');
+  divError.textContent = text;
+  para.appendChild(divError);
 }
